4c646d3ebb89542500b61f6621119d3aa40f23c4
Yusifov Telman <112288286+Rept1l1an@users.noreply.github.com>
2022-12-28 03:11:33 +0400

    DigitTest

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+// ANY
+string TestAny = Console.ReadLine();
+bool message = false;
+
+for (int i = 0; i < TestAny.Length; i++)
+{
+    if (char.IsDigit(TestAny[i]))
+    {
+        message = true;
+        break;
+    }
+}
+Console.WriteLine(message);
+
+//ALL
+string TestAll = Console.ReadLine();
+bool message = true;
+
+for(int i =0; i < TestAll.Length; i++)
+{
+    if (char.IsDigit(TestAll[i]) != true)
+    {
+        message = false;
+        break;
+    }
+}
+
+Console.WriteLine(message);
